5bcaea183045d2cb838640feb65c8c90fe313944
NYtmouse <154848583+NYtmouse@users.noreply.github.com>
2023-12-26 01:57:26 +0000

    config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['233593322727', 'me', true],
+  ['233551655899', 'james', true], 
   [''] 
 ] //Number of owners
 
